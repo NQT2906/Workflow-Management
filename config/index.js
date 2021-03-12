2aec22e148e8e81c30fba74fd7cdcd45e731f1e4
NQT2906 <nguyenquangthuan2906@gmail.com>
2021-03-13 03:03:47 +0700

    Config database

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var configValues = require('./config.json');
 
 module.exports = {
     getDbConnectionString: function() {
-        return `mongodb+srv://{ configValues.username } ${ configValues.password }@workflow-management.f2rqr.mongodb.net/test`;
+        return ``;
     }
 }

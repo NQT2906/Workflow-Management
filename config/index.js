ea1e81c4a21b63ba9b5caae0aa2174e846e7a75d
Admin <Admin@DESKTOP-6S0E0N7>
2021-03-13 02:54:11 +0700

    Config db

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var configValues = require('./config.json');
 
 module.exports = {
     getDbConnectionString: function() {
-        return `mongodb+srv://ThuanNQ:${ configValues.password }@workflow-management.f2rqr.mongodb.net/test`;
+        return `mongodb+srv://{ configValues.username } ${ configValues.password }@workflow-management.f2rqr.mongodb.net/test`;
     }
 }

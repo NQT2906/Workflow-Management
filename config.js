a2105bf8293c4f5b0e4b1a35cb6f26bc3cea24b6
Nguyễn Quang Thuận <nguyenquangthuan2906@gmail.com>
2021-05-01 11:41:34 +0700

    Fix callback url

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports={
     "facebook_api_key"      :     "316099329905754",
     "facebook_api_secret"   :     "fe296e5fdf12f564eb80dfbe9bac02fd",
-    // "callback_url"          :     "https://workflow-management-2k.herokuapp.com/auth/facebook/callback",
-    "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+    "callback_url"          :     "https://workflow-management-2k.herokuapp.com/auth/facebook/callback",
+    // "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
     "use_database"          :     true,
     "host"                  :     "localhost",
     "username"              :     "root",

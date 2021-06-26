5c2546cf5c3978967539f420ec2561c7d3fdbbb6
Nguyen Quang Thuan <nguyenquangthuan2906@gmail.com>
2021-06-26 11:02:04 +0700

    Change mongo db

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ var configValues = require('./config.json');
 
 module.exports = {
     getDbConnectionString: function() {
-        // return `mongodb+srv://ThuanNQ:${ configValues.password }@workflow-management.f2rqr.mongodb.net/test`;
-        return `mongodb://localhost:27017/User_Login_FB`;
+        return `mongodb+srv://ThuanNQ:${ configValues.password }@workflow-management.f2rqr.mongodb.net/test`;
+        // return `mongodb://localhost:27017/User_Login_FB`;
     },
     "facebook_api_key"      :     "162131665830933",
     "facebook_api_secret"   :     "bd939a16934343c8ebfe35537e3ae237",

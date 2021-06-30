263cb528e971bc8a1283addfb3a1a25cd8763fc7
Nguyen Quang Thuan <nguyenquangthuan2906@gmail.com>
2021-06-30 17:13:55 +0700

    Add deadline input

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports={
     "facebook_api_key"      :     "162131665830933",
     "facebook_api_secret"   :     "bd939a16934343c8ebfe35537e3ae237",
-    "callback_url"          :     "https://workflow-management-2k.herokuapp.com/auth/facebook/callback",
-    // "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+    // "facebook_api_key"      :     "525057695358273",
+    // "facebook_api_secret"   :     "78195e58725d0423ec0c9a0dc57a68bb",
+    // "callback_url"          :     "https://workflow-management-2k.herokuapp.com/auth/facebook/callback",
+    "callback_url"          :     "https://e727a945bdae.ngrok.io/auth/facebook/callback",
     "use_database"          :     true,
     "host"                  :     "localhost",
     "username"              :     "root",
